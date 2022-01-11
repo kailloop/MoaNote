f06c31d159287f203ebae978e369e549f653514d
master <82108@DESKTOP-SDBJJB4>
2022-01-11 15:22:34 +0900

    [2022.01.11.강범석] 1. AutoLogin 순서 구축

--- app/src/main/java/kr/co/moanote/view/activity/IntroActivity.kt
+++ app/src/main/java/kr/co/moanote/view/activity/IntroActivity.kt
@@ -1,12 +1,27 @@
 package kr.co.moanote.view.activity
 
+import android.content.Intent
 import android.view.View
 import kr.co.moanote.R
+import kr.co.moanote.databinding.ActivityIntroBinding
+import kr.co.moanote.presenter.Contract
+import kr.co.moanote.presenter.IntroPresenter
+import kr.co.moanote.util.Util
 import kr.co.moanote.view.BaseActivity
 
-class IntroActivity : BaseActivity() {
+class IntroActivity : BaseActivity(), Contract.View {
+
+    private val binding by lazy { ActivityIntroBinding.inflate(layoutInflater) }
+    private lateinit var presenter: IntroPresenter
+
+    // *****************************************************************************
+    // LIFE CYCLE
+    // *****************************************************************************
     override fun createActivity() {
         setContentView(R.layout.activity_intro)
+        presenter = IntroPresenter(this)
+
+        init()
     }
 
     override fun pauseActivity() {
@@ -21,7 +36,26 @@ class IntroActivity : BaseActivity() {
 
     }
 
+    // *****************************************************************************
+    // INIT UI
+    // *****************************************************************************
+    private fun init() {
+        goScreen()
+    }
+
+    // *****************************************************************************
+    // EVENT
+    // *****************************************************************************
     override fun viewClick(v: View?) {
 
+    }
+
+    // *****************************************************************************
+    // FUNC
+    // *****************************************************************************
+    private fun goScreen() {
+        val login = presenter.chkAutoLogin(this)
+
+        startActivity(Intent(this, login::class.java))
     }
 }
--- app/src/main/java/kr/co/moanote/view/activity/LoginActivity.kt
+++ app/src/main/java/kr/co/moanote/view/activity/LoginActivity.kt
@@ -1,11 +1,23 @@
 package kr.co.moanote.view.activity
 
 import android.view.View
+import kr.co.moanote.R
+import kr.co.moanote.presenter.Contract
+import kr.co.moanote.presenter.LoginPresenter
+import kr.co.moanote.util.Util
 import kr.co.moanote.view.BaseActivity
 
-class LoginActivity : BaseActivity(){
-    override fun createActivity() {
+class LoginActivity : BaseActivity(), Contract.View {
 
+    private lateinit var presenter: Contract.Presenter
+
+    // *****************************************************************************
+    // LIFE CYCLE
+    // *****************************************************************************
+    override fun createActivity() {
+        setContentView(R.layout.activity_login)
+        Util.MOA_LOG("On LoginActivity")
+        presenter = LoginPresenter(this)
     }
 
     override fun pauseActivity() {
@@ -20,6 +32,9 @@ class LoginActivity : BaseActivity(){
 
     }
 
+    // *****************************************************************************
+    // EVENT
+    // *****************************************************************************
     override fun viewClick(v: View?) {
 
     }
